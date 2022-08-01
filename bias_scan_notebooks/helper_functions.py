3b2b9ddcac41ba59a933acb26323debc2b4053e5
Jurriaan Parie <Jurriaan.Parie@ibm.com>
2022-08-01 10:59:59 +0200

    update

--- bias_scan_notebooks/helper_functions.py
+++ bias_scan_notebooks/helper_functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef37ce4794844c29b054b6cba0d26b179ddebb7b3c451024479ee152f4cf5af3
-size 8256
+oid sha256:74195b97d5b6592977132471ffb1fbd7b4292bc20888a3b614b54cfe73f7d32f
+size 8258
